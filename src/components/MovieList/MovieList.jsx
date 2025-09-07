43daccaeea546130e721b94a7d92b64f1942e0a3
abinayaprabu <abinaya2310921@ssn.edu.in>
2025-09-07 12:46:29 +0530

    Add files via upload

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import _ from 'lodash'
+
+import "./MovieList.css";
+import Fire from "../../assets/fire.png";
+import MovieCard from "./MovieCard";
+import FilterGroup from "./FilterGroup";
+
+
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const [minRating, setminRating] = useState(0);
+  const [filterMovies, setfilterMovies] = useState([]);
+  const [sort,setSort]=useState({
+    by:"default",
+    order:"asc"
+  }
+
+  )
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  useEffect(()=>{
+    if(sort.by !=="default"){
+      const sortedMovies=_.orderBy(filterMovies,[sort.by],[sort.order])
+      setfilterMovies(sortedMovies)
+    }
+  },[sort])
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=f564ebe4abd692a20dd65b9c340001d9"
+    );
+    const data = await response.json();
+    setMovies(data.results);
+    setfilterMovies(data.results);
+  };
+  const handleFilter = (rate) => {
+    if (rate === minRating) {
+      setminRating(0);
+      setfilterMovies(movies);
+    } else {
+      setminRating(rate);
+      
+
+      const filtered = movies.filter((movie) => movie.vote_average >= rate);
+      setfilterMovies(filtered);
+    }
+  };
+  const handleSort = e =>{
+    const {name,value} = e.target;
+    setSort(prev => 
+   ({...prev,[name]:value
+    }))
+  }
+  return (
+    <section className="movie_list">
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
+          Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
+        </h2>
+        <div className="align_center movie_list_fs">
+          <FilterGroup
+            minRating={minRating}
+            onRatingClick={handleFilter}
+            ratings={[8, 7, 6]}
+          />
+          <select name="by" id="" onChange={handleSort} value={
+            sort.by
+          }
+          className="movie_sorting">
+            <option value="default">SortBy</option>
+            <option value="release_date">Date</option>
+            <option value="vote_average">Rating</option>
+          </select>
+          <select name="order" id="" onChange={handleSort} value={sort.order} className="movie_sorting">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+      </header>
+      <div className="movie_cards">
+        {filterMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default MovieList;
